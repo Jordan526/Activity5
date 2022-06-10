c7f59dfab2b9e732b2549b384f552fd3c2e75c24
Jordan526 <96961388+Jordan526@users.noreply.github.com>
2022-06-09 21:39:37 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Activity5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userFileTextBox = new System.Windows.Forms.RichTextBox();
+            this.readFileButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // userFileTextBox
+            // 
+            this.userFileTextBox.Location = new System.Drawing.Point(53, 12);
+            this.userFileTextBox.Name = "userFileTextBox";
+            this.userFileTextBox.Size = new System.Drawing.Size(517, 210);
+            this.userFileTextBox.TabIndex = 0;
+            this.userFileTextBox.Text = "";
+            // 
+            // readFileButton
+            // 
+            this.readFileButton.Location = new System.Drawing.Point(241, 228);
+            this.readFileButton.Name = "readFileButton";
+            this.readFileButton.Size = new System.Drawing.Size(144, 38);
+            this.readFileButton.TabIndex = 1;
+            this.readFileButton.Text = "Select File For Input";
+            this.readFileButton.UseVisualStyleBackColor = true;
+            this.readFileButton.Click += new System.EventHandler(this.readFileButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 357);
+            this.Controls.Add(this.readFileButton);
+            this.Controls.Add(this.userFileTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox userFileTextBox;
+        private System.Windows.Forms.Button readFileButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void readFileButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string userFileName = openFileDialog1.FileName;
+            string readFile = File.ReadAllText(userFileName);
+            userFileTextBox.Text += readFile.ToLower();
+            StreamReader reader = new StreamReader(userFileName);
+            string[] words = reader.ReadToEnd().Split();
+            string longestWord = "";
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > longestWord.Length)
+                {
+                    longestWord = words[i];
+                }
+            }
+           
+            userFileTextBox.Text += "\n" + "\n" +  "Longest Word: " + longestWord;
+            userFileTextBox.Text += "\n" + "First Word: " + words[0].ToLower() + "\n";
+            userFileTextBox.Text += "Last Word: " + words[words.Length - 1] + "\n";
+           
+        }
+    }
+}
+
